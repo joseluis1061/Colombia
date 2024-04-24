67f11473c156ff88cbb1f8af1da6bbfc3e4ec2b3
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-24 17:57:48 -0500

    Se crean los modulos y rutas de acceso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/webside/webside.module').then(m => m.WebsideModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./modules/administrador/administrador.module').then(m => m.AdministradorModule)
+  }
+];
