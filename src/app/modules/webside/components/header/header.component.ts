b3dcc31c0413c1703ab70ff85041fd4d6d46ef94
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-26 17:53:29 -0500

    Add header component

--- src/app/modules/webside/components/header/header.component.ts
+++ src/app/modules/webside/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faTable } from '@fortawesome/free-solid-svg-icons';
+import { faTableList } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faCoffee = faCoffee;
+  faHome = faHome;
+  faTable = faTable;
+  faTableList = faTableList;
+  faUser = faUser;
 }
