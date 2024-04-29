bf7d9eaea7a09d72f9323375e2ccac585f5f350b
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-28 19:20:51 -0500

    Se extiende modelo de Deparments para poder traer la lista completa de departamentos

---
+++ src/app/shared/constants/regions-image.const.ts
@@ -0,0 +1,26 @@
+export const REGIONS_IMAGE = [
+  {
+    id: '1',
+    url: '../../../assets/img/regions/region_caribe.jpg'
+  },
+  {
+    id: '2',
+    url: '../../../assets/img/regions/region_pacifico.jpg'
+  },
+  {
+    id: '3',
+    url: '../../../assets/img/regions/region_orinoquia.jpg'
+  },
+  {
+    id: '4',
+    url: '../../../assets/img/regions/region_amazonia.jpg'
+  },
+  {
+    id: '5',
+    url: '../../../assets/img/regions/region_andina.jpg'
+  },
+  {
+    id: '6',
+    url: '../../../assets/img/regions/region_insular.jpg'
+  }
+]
