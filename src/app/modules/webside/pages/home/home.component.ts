36fb23d1c15c9aedd8e1dd816435e92131e5ae8d
José Luis Zúñiga <jose@tangibledesign.co>
2024-05-10 21:40:50 -0500

    Se agrega página de resumen del restaurante

--- src/app/modules/webside/pages/home/home.component.ts
+++ src/app/modules/webside/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHotel } from '@fortawesome/free-solid-svg-icons'
-import { faUtensils } from '@fortawesome/free-solid-svg-icons'
-import { faPlane } from '@fortawesome/free-solid-svg-icons'
-import { faRoute } from '@fortawesome/free-solid-svg-icons'
-import { faPersonWalkingLuggage } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faHotel } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faPlane } from '@fortawesome/free-solid-svg-icons';
+import { faRoute } from '@fortawesome/free-solid-svg-icons';
+import { faPersonWalkingLuggage } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   standalone: true,
--- src/app/modules/webside/pages/restaurants/components/restaurant/restaurant.component.ts
+++ src/app/modules/webside/pages/restaurants/components/restaurant/restaurant.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardFoodComponent } from '../card-food/card-food.component';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-restaurant',
   standalone: true,
-  imports: [CommonModule, CardFoodComponent],
+  imports: [CommonModule, CardFoodComponent, FontAwesomeModule],
   templateUrl: './restaurant.component.html',
 })
 export class RestaurantComponent {
+  faLocationDot = faLocationDot;
   ngOnInit(): void { }
 
 }
