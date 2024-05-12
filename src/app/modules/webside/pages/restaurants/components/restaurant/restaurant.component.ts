6ca906c890d5ba31edad1b4eaa3086e0ebfba3b3
jose luis zuñiga <joseluis836@hotmail.com>
2024-05-11 21:04:21 -0500

    Se agregan estrellas de votación

--- src/app/modules/webside/pages/restaurants/components/restaurant/restaurant.component.ts
+++ src/app/modules/webside/pages/restaurants/components/restaurant/restaurant.component.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import { CardFoodComponent } from '../card-food/card-food.component';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StarRatingComponentComponent } from '../../../../../../shared/components/star-rating-component/star-rating-component.component';
+// import { StarRatingConfigService } from 'angular-star-rating';
+// import { StarRatingModule } from 'angular-star-rating';
+
 @Component({
   selector: 'app-restaurant',
   standalone: true,
-  imports: [CommonModule, CardFoodComponent, FontAwesomeModule],
+  imports: [CommonModule, CardFoodComponent, FontAwesomeModule, StarRatingComponentComponent], //, StarRatingModule
   templateUrl: './restaurant.component.html',
+  // providers: [StarRatingConfigService]
 })
 export class RestaurantComponent {
   faLocationDot = faLocationDot;
+  myRating = 0;
+  
   ngOnInit(): void { }
+
+
+  onRatingChange(rating: number) {
+    this.myRating = rating;
+    console.log(`La nueva calificación es ${rating}`);
+  }
 
 }
