9cdd38627c80a220758ee95a1f7c22597d101237
José Luis Zúñiga <jose@tangibledesign.co>
2024-05-17 17:58:17 -0500

    Add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideFirebaseApp(() => initializeApp({"projectId":"turismo-f9aa8","appId":"1:691653797325:web:815c932aad0905c83fd084","storageBucket":"turismo-f9aa8.appspot.com","apiKey":"AIzaSyDXBR1rIFiXlcTl62M2e6RtbJXCeork4Yc","authDomain":"turismo-f9aa8.firebaseapp.com","messagingSenderId":"691653797325"})), provideAuth(() => getAuth()), provideStorage(() => getStorage())]
 };
