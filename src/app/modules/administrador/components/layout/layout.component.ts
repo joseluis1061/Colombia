1c0fdea3090fedbb9bbadd7a0cd3d62f9661bc43
José Luis Zúñiga <jose@tangibledesign.co>
2024-06-21 17:42:07 -0500

    Add button anunciar and notification

--- src/app/modules/administrador/components/layout/layout.component.ts
+++ src/app/modules/administrador/components/layout/layout.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FontAwesomeModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  faBell = faBell;
 
 }
