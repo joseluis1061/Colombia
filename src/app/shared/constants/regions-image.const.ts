f4677a53b272a7f2f7fc25bd0786b806ff0c96eb
José Luis Zúñiga <jose@tangibledesign.co>
2024-06-23 21:37:11 -0500

    Se agregan imagenes base a firestore

--- src/app/shared/constants/regions-image.const.ts
+++ src/app/shared/constants/regions-image.const.ts
@@ -1,26 +1,26 @@
 export const REGIONS_IMAGE = [
   {
     id: '1',
-    url: '../../../assets/img/regions/region_caribe.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_caribe.jpg?alt=media&token=75a6901d-2c98-4e71-b060-398b73a4f57f'
   },
   {
     id: '2',
-    url: '../../../assets/img/regions/region_pacifico.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_pacifico.jpg?alt=media&token=f5b5d0dc-1368-4214-b8a7-9f6990f70549'
   },
   {
     id: '3',
-    url: '../../../assets/img/regions/region_orinoquia.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_orinoquia.jpg?alt=media&token=1681e23a-2848-4de9-8087-6d343485aa12'
   },
   {
     id: '4',
-    url: '../../../assets/img/regions/region_amazonia.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_amazonia.jpg?alt=media&token=5f111af0-dade-48d7-b8a8-08e802b954bb'
   },
   {
     id: '5',
-    url: '../../../assets/img/regions/region_andina.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_andina.jpg?alt=media&token=fef76431-9dd8-427a-8a83-b1e896d9c77a'
   },
   {
     id: '6',
-    url: '../../../assets/img/regions/region_insular.jpg'
+    url: 'https://firebasestorage.googleapis.com/v0/b/turismocomunitario-173e7.appspot.com/o/assets%2Fhome%2Fregiones%2Fregion_insular.jpg?alt=media&token=b97b573b-7f4a-4cc6-819b-11e4efe9d943'
   }
 ]
